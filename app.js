d90d19e718f13f73c5c7237990c682a5b198e831
KabirKang <kangkabir@gmail.com>
2018-07-11 23:00:25 -0700

    Add albums view

--- app.js
+++ app.js
@@ -23,23 +23,29 @@ var albums = require('./routes/albums');
 var genres = require('./routes/genres');
 var users = require('./routes/users');
 
+// Init app
 var app = express();
 
+// View engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Logger
 app.use(logger('dev'));
 
+// Body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Handle Sessions
 app.use(session({
   secret: 'secret',
   resave: true,
   saveUninitialized: true
 }));
 
+// Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
     var namespace = param.split('.'),
@@ -56,10 +62,13 @@ app.use(expressValidator({
   }
 }));
 
+// Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Connect flash
 app.use(flash());
 
+// Global Vars
 app.use(function(req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
@@ -67,13 +76,16 @@ app.use(function(req, res, next) {
   next();
 });
 
+// Routes
 app.use('/', routes);
 app.use('/albums', albums);
 app.use('/genres', genres);
 app.use('/users', users);
 
+// Set Port
 app.set('port', (process.env.PORT || 3000));
 
+// Run server
 app.listen(app.get('port'), function() {
   console.log('Server started on port: ' + app.get('port'));
 });
